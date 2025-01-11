21fff97f46b7d281389a8d96dea7f893331a67fd
harsh.kumar1 <harsh.kumar1@phonepe.com>
2025-01-11 14:57:30 +0530

    corrected unit tests job

--- app/src/main/java/com/example/movieapp/dummyclass.kt
+++ app/src/main/java/com/example/movieapp/dummyclass.kt
@@ -3,8 +3,8 @@ package com.example.movieapp
 class Dummy {
 
      fun dummyFunction( x : Int, y : Int) : Int{
-        val newX = x*2
-        val newY = y*2
-         return newY+newY
+        val newX = x*20
+        val newY = y*20
+         return newY+newY+10
     }
 }
