9cc0a39ba5fd348f8f3cdcfb6fbe95a6a1b0b5a8
harsh.kumar1 <harsh.kumar1@phonepe.com>
2025-01-11 16:43:49 +0530

    corrected unit tests job

--- app/src/main/java/com/example/movieapp/dummyclass.kt
+++ app/src/main/java/com/example/movieapp/dummyclass.kt
@@ -5,24 +5,24 @@ class Dummy {
      fun dummyFunction( x : Int, y : Int) : Int{
         val newX = x*20
         val newY = y*20
-         return newY+newY+10
+         return newX+newY+10
     }
 
     fun dummyFunction2( x : Int, y : Int) : Int{
         val newX = x*20
         val newY = y*20
-        return newY+newY+10
+        return newX+newY+10
     }
 
     fun dummyFunction3( x : Int, y : Int) : Int{
         val newX = x*20
         val newY = y*20
-        return newY+newY+10
+        return newX+newY+10
     }
 
     fun dummyFunction4( x : Int, y : Int) : Int{
         val newX = x*20
         val newY = y*20
-        return newY+newY+10
+        return newX+newY+10
     }
 }
