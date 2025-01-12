cca9e2106d98c5200ac0b7f17b0039d2454ff5ff
harsh.kumar1 <harsh.kumar1@phonepe.com>
2025-01-12 10:59:00 +0530

    corrected unit tests job

--- app/src/main/java/com/example/movieapp/dummyclass.kt
+++ app/src/main/java/com/example/movieapp/dummyclass.kt
@@ -27,12 +27,4 @@ class Dummy {
     fun dummyFunction6( x : Int, y : Int) : Int{
         return x+x+y+x
     }
-
-    fun dummyFunction7( ) : Int{
-        return 3+3+3+5
-    }
-
-    fun dummyFunction8( x : Int, y : Int) : Int{
-        return 898+x+y+x
-    }
 }
